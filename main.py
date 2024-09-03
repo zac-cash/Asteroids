332ec04c06e6f6792e09a6bd2a6e38969893186a
zac-cash <Personal@zac.cash>
2024-09-03 16:26:22 -0500

    take 2: let's get a boogie goin.

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import  *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=0)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
